d8ae5d534add5668ca9434a79417912003618946
bchetcuti <54513583+bchetcuti@users.noreply.github.com>
2025-10-20 19:37:19 +1100

    initial commit

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bc-cache-v3';
+const ASSETS=['/','/index.html','/style.css','/assets/bc-mark.svg','/pages/about.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>{if(e.request.mode==='navigate'){return new Response('<html><body><h2>Offline</h2><p>You appear to be offline. Please try again later.</p></body></html>',{headers:{'Content-Type':'text/html'}});}})));});
